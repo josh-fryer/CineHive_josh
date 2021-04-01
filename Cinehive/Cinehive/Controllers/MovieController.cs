d5be09c16b2cc7d8e9bfd757db63f7e2d5001b73
liljoshcodes <josh.fryer2@gmail.com>
2021-04-01 21:22:08 +0100

    added api connection with methods in new Movie controller
    + can view retrieved movie data in ViewMovie()
    + can search for a film by title through api.
    + added movie model

---
+++ Cinehive/Cinehive/Controllers/MovieController.cs
@@ -0,0 +1,93 @@
+﻿using HiveData.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinehive.Controllers
+{
+    public class MovieController : Controller
+    {
+        string key = "f843620fb54518820f6b817e854c9ce3";
+        Uri baseAddress = new Uri("https://api.themoviedb.org/3/");
+        HttpClient client;
+
+        public MovieController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+
+        // view movie details page
+        public ActionResult ViewMovie(string query)
+        {
+            string q = PrepareQuery(query);
+            dynamic i = SearchMovie(q);
+            if(i == null )
+            {
+                return View(); // display empty
+            }
+            else
+            {
+                return View(ConvertToMovie(i.results[0])); // returns first result from api's search results
+            }          
+        }
+
+        
+        dynamic SearchMovie(string query)
+        {
+            // example: "https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher"
+            HttpResponseMessage response =
+                client.GetAsync(client.BaseAddress + "search/movie?api_key="+key+"&query="+query).Result;
+            if (!response.IsSuccessStatusCode) // call not successful
+            {               
+                return null; // display nothing
+            }
+            else
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                // Deserialise JSON string into dynamic object
+                dynamic item = JsonConvert.DeserializeObject<dynamic>(data);
+
+                return item; // returns array of search results
+            }
+        }
+
+        private string PrepareQuery(string query)
+        {
+            // prepare search query for api
+            //string input = "Jack Reacher";
+            string input = query.Trim();
+            string newQ = "";
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    newQ += '+';
+                }
+                else
+                {
+                    newQ += c;
+                }
+            }
+            return newQ;
+        }
+
+        Movie ConvertToMovie(dynamic item)
+        {
+            Movie movie = new Movie()
+            {
+                id = item.id,
+                poster_path = item.poster_path,
+                overview = item.overview,
+                title = item.title,
+                release_date = item.release_date              
+            };
+            return movie;
+        }
+
+    }
+}
---
+++ Cinehive/HiveData/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiveData.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string poster_path { get; set; }
+        public string overview { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime release_date { get; set; }
+        public int[] genre_ids { get; set; }
+        public string title { get; set; }
+
+    }
+}
