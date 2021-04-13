afb46dffda37851faa0de1989746684485dcd87f
liljoshcodes <josh.fryer2@gmail.com>
2021-04-13 14:26:50 +0100

    React install with JSX and Node installed.

--- Cinehive/Cinehive/Content/CinehiveScript.js
+++ Cinehive/Cinehive/Content/CinehiveScript.js
@@ -1,20 +1,21 @@
-﻿function validateForm() {
+function validateForm() {
     var x = document.forms["createPost"]["input"].value;
-    if (x == "") { // if post text area is empty then do not submit
+    if (x == "") {
+        // if post text area is empty then do not submit
         alert("Post cannnot be empty!");
         return false;
     }
 }
 
 // ############### Char Remaining Counter ##################
-const textArea = document.getElementById("postInput");
-const remainngCharsText = document.getElementById("remaining-chars");
+var textArea = document.getElementById("postInput");
+var remainngCharsText = document.getElementById("remaining-chars");
 var maxLength = textArea.getAttribute("maxlength");
 remainngCharsText.textContent = "280 characters remaining";
-textArea.addEventListener("input", () => {
+textArea.addEventListener("input", function () {
 
-    const remaining = maxLength - textArea.value.length;
-    remainngCharsText.textContent = `${remaining} characters remaining`;
+    var remaining = maxLength - textArea.value.length;
+    remainngCharsText.textContent = remaining + " characters remaining";
 });
 // ###########################################################
 
---
+++ Cinehive/Cinehive/Content/Scripts/CinehiveScript.js
@@ -0,0 +1,34 @@
+﻿function validateForm() {
+    var x = document.forms["createPost"]["input"].value;
+    if (x == "") { // if post text area is empty then do not submit
+        alert("Post cannnot be empty!");
+        return false;
+    }
+}
+
+// ############### Char Remaining Counter ##################
+const textArea = document.getElementById("postInput");
+const remainngCharsText = document.getElementById("remaining-chars");
+var maxLength = textArea.getAttribute("maxlength");
+remainngCharsText.textContent = "280 characters remaining";
+textArea.addEventListener("input", () => {
+
+    const remaining = maxLength - textArea.value.length;
+    remainngCharsText.textContent = `${remaining} characters remaining`;
+});
+// ###########################################################
+
+// ################ Show/hide div ############################
+
+//function showHide(id) {
+//    var x = document.getElementById(id);
+
+//    if (x.style.display
+//        === "block") {
+//        x.style.display = "none";
+//    } else {
+//        x.style.display =
+//            "block";
+//    }
+//};
+//###############################################################
